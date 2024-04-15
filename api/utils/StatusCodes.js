af4129a317953cd9166573063fef12f22065f407
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-15 12:24:37 +0200

    Status Codes and Error Hnadlers

---
+++ api/utils/StatusCodes.js
@@ -0,0 +1,10 @@
+exports.constants = {
+    SUCCESS: 200,
+    FOUND: 302,
+    VALIDATION_ERROR: 400,
+    UNAUTHORIZED: 401,
+    FORBIDEN: 403,
+    NOT_FOUND : 404,
+    SERVER_ERROR: 500,
+    SERVICE_ANAVAILABLE: 503,
+};
