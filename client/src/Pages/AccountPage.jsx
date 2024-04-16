61a0041da388dc23df6cf99160b60a61b0010547
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-16 18:19:39 +0200

    Account Page

---
+++ client/src/Pages/AccountPage.jsx
@@ -0,0 +1,92 @@
+import { useContext, useState } from "react"
+import { UserContext } from "../context/UserContext"
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+import PlacesPage from "./PlacesPage";
+
+
+const AccountPage = () => {
+
+  const {user, ready, setuser} = useContext(UserContext);
+ const [redirect, setRedirect] = useState(null)
+
+  let {subpage} = useParams();
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+
+
+  async function Logout() {
+   await axios.post('/logout');
+    setuser(null);
+    setRedirect('/');
+  }
+
+  if (!ready) {
+    return 'Loading...'
+  }
+
+  if (ready && !user && !redirect) {
+    return <Navigate to={'/login'}/>
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect}/>
+  }
+
+
+  function linkClasses(type= null){
+     let classes = 'py-2 px-6 inline-flex gap-1 rounded-full';
+     if (type === subpage ) {
+      classes += 'bg-primary text-white ';
+     }else{
+      classes+= 'bg-gray-200'
+     }
+     return classes;
+  }
+
+  return (
+    <div>
+        <nav className="w-full flex mt-8 gap-2 justify-center mb-8">
+          <Link className={linkClasses('profile')} to={'/account'}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+            My Profile</Link>
+          <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+</svg>
+
+       My Bookings</Link>
+          <Link className={linkClasses('places')} to={'/account/places'}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 0 0-8.862 12.872M12.75 3.031a9 9 0 0 1 6.69 14.036m0 0-.177-.529A2.25 2.25 0 0 0 17.128 15H16.5l-.324-.324a1.453 1.453 0 0 0-2.328.377l-.036.073a1.586 1.586 0 0 1-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 0 1-5.276 3.67m0 0a9 9 0 0 1-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+</svg>
+My Accomodations</Link>
+        </nav>
+
+        { subpage == 'profile' &&(
+          <div className="text-center max-w-lg mx-auto">
+           logged in as {user.name} ({user.email}) <br />
+           <button onClick={Logout} className="primary max-w-sm mt-2">Logout</button>
+          </div>
+        )}
+
+         { subpage == 'places' &&(
+          <div className="text-center max-w-lg mx-auto">
+           <PlacesPage/>
+          </div>
+        )}
+
+        { subpage == 'bookings' &&(
+          <div className="text-center max-w-lg mx-auto">
+         
+          </div>
+        )}
+
+    </div>
+  )
+}
+
+export default AccountPage
