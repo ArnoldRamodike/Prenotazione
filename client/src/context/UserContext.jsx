dc6c13981719c5ac61ea000bc59485eb43ba0964
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-16 18:19:10 +0200

    Routing and styles upldates

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -5,17 +5,20 @@ export const UserContext = createContext({})
 
 
 export function UserContextProvider({children}){
+    
     const [user, setuser] = useState(null);
+    const [ready, setReady] = useState(false);
     useEffect(() => {
         if (!user) {
            axios.get('/profile').then(({data}) => {
               setuser(data);
+              setReady(true);
            })   
         }
     }, [])
 
     return (
-        <UserContext.Provider value={{user,setuser}}>
+        <UserContext.Provider value={{user,setuser, ready}}>
             {children}
         </UserContext.Provider>
         
