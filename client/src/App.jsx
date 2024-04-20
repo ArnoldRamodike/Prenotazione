81a016f1a47dcbbc84de6caaecb6a50feba8d57b
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-20 23:30:26 +0200

    code fixes for place

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { UserContextProvider } from './context/UserContext'
 import ProfilePage from './Pages/ProfilePage'
 import PlacesPage from './Pages/PlacesPage'
 import PlacesForm from './forms/PlacesForm'
+import PlacePage from './Pages/PlacePage'
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials= true;
@@ -28,6 +29,7 @@ function App() {
           <Route path='/account/places' element={<PlacesPage/>}/>
           <Route path='/account/places/new' element={<PlacesForm/>}/>
           <Route path='/account/places/:id' element={<PlacesForm/>}/>
+          <Route path='/place/:id' element={<PlacePage/>}/>
         </Route>
       </Routes>
           
--- client/src/forms/PlacesForm.jsx
+++ client/src/forms/PlacesForm.jsx
@@ -15,7 +15,8 @@ const PlacesForm = () => {
     const [extraInfo, setExtraInfo] = useState('');
     const [checkIn, setCheckIn] = useState('');
     const [checkOut, setCheckOut] = useState('');
-    const [maxGuests, setMaxGuests] = useState('');
+    const [maxGuest, setMaxGuests] = useState(1);
+    const [price, setPrice] = useState(100);
     const [redirect, setRedirect] = useState(false);
 
 
@@ -34,7 +35,8 @@ const PlacesForm = () => {
         setPerks(data.perks);
         setCheckIn(data.checkIn);
         setCheckOut(data.checkOut);
-        setMaxGuests(data.maxGuests);
+        setMaxGuests(data.maxGuest);
+        setPrice(data.price);
       });
 
     }, [id])
@@ -63,7 +65,7 @@ const PlacesForm = () => {
       ev.preventDefault();
       const placeData = {title, address, addedPhotos, 
         description, perks, extraInfo, 
-        checkIn, checkOut, maxGuests}
+        checkIn, checkOut, maxGuest, price}
       if (id) {
         //Update functiionality
         await axios.put(`/places/${id}`, {
@@ -99,7 +101,7 @@ const PlacesForm = () => {
         {preInput('Extra Informaton', 'More relevent information for the place')}
         <textarea className="" placeholder="Optional" value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)}/>
         {preInput('Check in&out', 'How long you going to stay')}
-        <div className="grid sm:grid-cols-3 gap-2">
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-2">
           <div className="">
             <h3 className="mt-2 -mb-2">Check in</h3>
             <input type="text" placeholder="18:00" value={checkIn} onChange={ev => setCheckIn(ev.target.value)}/>
@@ -110,7 +112,11 @@ const PlacesForm = () => {
           </div>
           <div className="">
           <h3 className="mt-2 -mb-2">Max Guests</h3>
-            <input type="number" placeholder="1" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)}/>
+            <input type="number" placeholder="1" value={maxGuest} onChange={ev => setMaxGuests(ev.target.value)}/>
+          </div>  
+          <div className="">
+            <h3 className="mt-2 -mb-2">Price per night</h3>
+            <input type="number" placeholder="1" value={price} onChange={ev => setPrice(ev.target.value)}/>
           </div>  
         </div>
         <button className="primary my-4">{id? 'Update': 'Save'}</button>
