24e9fe81f2fd0fe769274a2474ac8d3bff5f84ea
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-22 10:22:15 +0200

    updates

--- client/src/Pages/IndexPage.jsx
+++ client/src/Pages/IndexPage.jsx
@@ -15,7 +15,7 @@ const IndexPage = () => {
   return (
     <div className="mt-8 gap-x-6 gap-y-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {places.length > 0 && places.map(place => (
-          <Link to={'/place/'+place._id} key={place.id} className="">
+          <Link to={'/place/'+place._id} key={place._id} className="">
             <div className="bg-gray-500 rounded-2xl flex mb-2 cursor-pointer">
               {place.photos?.[0] && (
               <img className="rounded-2xl aspect-square object-cover" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="Place avatar"/>
