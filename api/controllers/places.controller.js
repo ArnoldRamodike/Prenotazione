f95e1f7b6bd59978dffe304fcade137a40708eec
ArnoldRamodike <riccoarnold23@gmail.com>
2025-04-28 22:39:11 +0200

    Remove the jwt token on Controller

--- api/controllers/places.controller.js
+++ api/controllers/places.controller.js
@@ -10,13 +10,10 @@ mongoose.connect(process.env.MONGODB_URL);
 
 const createPlaces = asyncHandler( async (req, res) => {
     const {title, address, addedPhotos,description,perks,extraInfo, checkIn, checkOut, maxGuest  } = req.body;
-    const { token} = req.cookies;
     try {
-            jwt.verify(token, jwtSecrete, {}, async(err, user) => {
-                if (err) throw err;
-                if ( !address || !description || !checkIn || !checkOut) {
-                    res.status(statuscode.BAD_GATEWAY).json("Address , Description, check in and Check out are reqired fields.");
-                }else{
+            if ( !address || !description || !checkIn || !checkOut) {
+                res.status(statuscode.BAD_GATEWAY).json("Address , Description, check in and Check out are reqired fields.");
+            }else{
                 const placeDoc = await Place.create({
                    owner: user.id,
                    title, address, 
@@ -24,28 +21,24 @@ const createPlaces = asyncHandler( async (req, res) => {
                    description,perks,
                    extraInfo, checkIn, 
                    checkOut, maxGuest
-                })
+                });
                 res.status(statuscode.CREATED).json(placeDoc);
             }
-            });
+           
     } catch (error) {
         res.status(statuscode.INTERNAL_SERVER_ERROR).json(error);
     }
 })
 
 const getPlaces = asyncHandler( async (req, res) => {
-    const { token} = req.cookies;
-        jwt.verify(token, jwtSecrete, {}, async(err, user) => {
-            if (err) throw err;
-            const {id} = user;
-            try {
-                const places =  await Place.find({owner: id});
-                res.status(statuscode.SUCCESS).json(places);
-            } catch (error) {
-                res.status(statuscode.INTERNAL_SERVER_ERROR).json(error);
-            }
-           
-        })
+        try {
+            const places =  await Place.find();
+            if(!places)
+                res.status(statuscode.BAD_GATEWAY).json("There are no available places Currenty, Please try again later.");
+            res.status(statuscode.SUCCESS).json(places);
+        } catch (error) {
+            res.status(statuscode.INTERNAL_SERVER_ERROR).json(error);
+        }
 });
 
 const getPlace = asyncHandler(  async (req, res) => {
@@ -61,13 +54,10 @@ const getPlace = asyncHandler(  async (req, res) => {
 
 const updatelaces = asyncHandler(  async (req, res) => {
     const {id, title, address, addedPhotos,description,perks,extraInfo, checkIn, checkOut, maxGuest  } = req.body;
-    const { token} = req.cookies;
 
     try {
-            jwt.verify(token, jwtSecrete, {}, async(err, user) => {
-                if (err) throw err;
-                const placeDoc = await Place.findById(id);
-                if (user.id === placeDoc.owner.toString()) {
+        const placeDoc = await Place.findById(id);
+            if (user.id === placeDoc.owner.toString()) {
                  placeDoc.set({
                    title, 
                    address, 
@@ -82,7 +72,6 @@ const updatelaces = asyncHandler(  async (req, res) => {
                 await placeDoc.save();
                 res.status(statuscode.SUCCESS).json('Place updated successfully');
              }   
-            })
     } catch (error) {
         res.status(statuscode.INTERNAL_SERVER_ERROR).json(error);
     }
