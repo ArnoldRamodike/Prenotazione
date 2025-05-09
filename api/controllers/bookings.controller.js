3ed29f06556e52e8c31df2358bd3efc2042271f4
ArnoldRamodike <riccoarnold23@gmail.com>
2025-05-09 21:10:21 +0200

    Fix booking and viewing functionalities

--- api/controllers/bookings.controller.js
+++ api/controllers/bookings.controller.js
@@ -11,26 +11,24 @@ mongoose.connect(process.env.MONGODB_URL);
 const createBooking = asyncHandler( async (req, res) => {
     const user = await getUserDataFromReq(req);
     const {place, checkIn, checkOut, numberOfGuests, name, phone, price } = req.body;
-    const { token} = req.cookies;
+
     try {
-            jwt.verify(token, jwtSecrete, {}, async(err, user) => {
-                if (err) throw err;
             if (!place || !name || !price || !name) {
                 res.status(statuscode.BAD_GATEWAY).json("Name, Price and Place are reqired fields.");
             }else{
                 const booking = await Booking.create({
                    name: name,
                    phone: phone,
-                   user: user,
+                   user: user.id,
                    place: place,
                    checkIn: checkIn,
                    checkOut: checkOut,
                    numberOfGuests: numberOfGuests,
                    price: price
-                })
+                });      
+
                 res.status(statuscode.CREATED).json(booking);
             }
-            });
             
     } catch (error) {
         res.status(statuscode.INTERNAL_SERVER_ERROR).json(error);
@@ -41,8 +39,8 @@ const getBookings = asyncHandler( async (req, res) => {
     const user = await getUserDataFromReq(req);
     const {id} = user;
     try {
-        const bookingDoc =  await Booking.find().populate('place');
-        res.json(bookingDoc);
+        const bookingDoc =  await Booking.find({user: id}).populate('place');
+        res.status(statuscode.SUCCESS).json(bookingDoc);
     } catch (error) {
         res.status(statuscode.INTERNAL_SERVER_ERROR).json(error)
     }
@@ -55,7 +53,7 @@ const getBooking = asyncHandler( async (req, res) => {
 
     try {
         const booking =  await Booking.find({user: id}).populate('place');
-        res.json(booking);
+        res.status(statuscode.SUCCESS).json(booking);
     } catch (error) {
         res.status(statuscode.INTERNAL_SERVER_ERROR).json(error)
     }
