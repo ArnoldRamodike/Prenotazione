9805b3e8ead187ac0a4058e973ff77401e3135f2
ArnoldRamodike <riccoarnold23@gmail.com>
2025-05-10 16:58:43 +0200

    Fix the local upload functionality

--- api/controllers/upload.controller.js
+++ api/controllers/upload.controller.js
@@ -5,12 +5,11 @@ const fs = require('fs');
 const path = require('path');
 const {statuscode} = require('../utils/StatusCodes');
 
-const photosMiddleware = multer({dest:'uploads/'});
+// const photosMiddleware = multer({dest:'uploads/'});
 const uploadDir = path.join('D:', 'dev', 'Skhu Dev', 'Prenotazione', 'api', 'uploads');
 
 const UploadByLink = asyncHandler(async (req, res) => {
     const {link} = req.body;
-    console.log("Link :", link);
    
     const newName = 'photo' + Date.now()+'.jpg';
     try {
@@ -26,10 +25,17 @@ const UploadByLink = asyncHandler(async (req, res) => {
 })
 
 
-const UploadPicture = asyncHandler( photosMiddleware.array('photos', 100), 
+const UploadPicture = asyncHandler( 
     async (req, res) => {
+        const files = req.files; 
+    // Check if files are present
+    if (!files || files.length === 0) {
+        return res.status(statuscode.NOT_FOUND).json({ message: 'No files uploaded' });
+    }
         const uploadedFiles= [];
-        for (let i = 0; i < req.files.length; i++) {
+  
+        try {
+            for (let i = 0; i < req.files.length; i++) {
             const {path, originalname} = req.files[i];
             const parts = originalname.split('.');
             const ext = parts[parts.length - 1];
@@ -37,7 +43,12 @@ const UploadPicture = asyncHandler( photosMiddleware.array('photos', 100),
             fs.renameSync(path, newpath);
             uploadedFiles.push(newpath.replace('uploads\\', ''));
         }
-    res.json(uploadedFiles);
+        res.status(statuscode.SUCCESS).json(uploadedFiles);
+        } catch (err) {
+            res.status(statuscode.INTERNAL_SERVER_ERROR).json({message: err})
+        }
+        
 })
+
 
 module.exports = {UploadByLink, UploadPicture }
--- api/routes/upload.routes.js
+++ api/routes/upload.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require('multer');
 const {UploadByLink, UploadPicture} = require("../controllers/upload.controller");
 
-router.route("/").post(UploadPicture);
+// Configure multer middleware
+const photosMiddleware = multer({ dest: 'uploads/' });
+// router.route("/").post(UploadPicture);
+router.route("/").post(photosMiddleware.array('photos', 100), UploadPicture);
 router.route("/upload-by-link").post(UploadByLink);
 
 module.exports = router;
